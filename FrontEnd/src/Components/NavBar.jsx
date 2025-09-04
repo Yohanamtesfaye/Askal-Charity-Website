8e72e9f6898d2e2207fab1a72c24937ff3295750
tsega7659 <yeabsirazelalem791@gmail.com>
2025-09-04 23:48:40 +0300

    remove logo comment from Navbar component

--- FrontEnd/src/Components/NavBar.jsx
+++ FrontEnd/src/Components/NavBar.jsx
@@ -20,7 +20,6 @@ const Navbar = () => {
     <nav className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
-          {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="flex items-center">
               <img
